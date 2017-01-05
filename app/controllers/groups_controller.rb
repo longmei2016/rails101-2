3041d70246cecee1b129b717cca041fc4c1ef500
mei <longmei2017@gmail.com>
2017-01-05 15:53:15 +0800

    implement groups#show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,15 +3,23 @@ class GroupsController < ApplicationController
     @groups = Group.all
   end
 
+  def show
+    @group = Group.find(params[:id])
+  end
+  # show action,表单位于app/views/groups/show.html.erb
+
   def new
     @group = Group.new
   end
+# new action
+# new action 的表单 位于 app/views/groups/new.html.erb
 
   def create
     @group = Group.new(group_params)
     @group.save
       redirect_to groups_path
   end
+  # create action:  接收 new 里的表单送出的讯息，变成一笔存在资料库里的资料
 
   private
 
