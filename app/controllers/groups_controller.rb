87ab183681ad472c743e46c4b3588476a3fbcd7e
mei <longmei2017@gmail.com>
2017-01-06 14:34:29 +0800

    use before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy ]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy ]
 
   def index
     @groups = Group.all
@@ -11,10 +12,6 @@ class GroupsController < ApplicationController
   # show action,表单位于app/views/groups/show.html.erb
 
   def edit
-    @group = Group.find(params[:id])
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-    end
   end
 
   def new
@@ -35,7 +32,6 @@ class GroupsController < ApplicationController
   # create action:  接收 new 里的表单送出的讯息，变成一笔存在资料库里的资料
 
   def update
-    @group = Group.find(params[:id])
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
     else
@@ -45,13 +41,19 @@ class GroupsController < ApplicationController
   # update action:  接收 edit 里的表单送出的讯息，真正更新存在资料库里的资料。
 
   def destroy
-    @group = Group.find(params[:id])
     @group.destroy
     flash[:alert] = "Group deleted"
       redirect_to groups_path
   end
 
   private
+
+  def find_group_and_check_permission
+    @group = Group.find(params[:id])
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
 
   def group_params
     params.require(:group).permit(:title, :description)
