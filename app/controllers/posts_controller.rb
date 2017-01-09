c7a009d618329be19098d36d17387f704f2e5521
mei <longmei2017@gmail.com>
2017-01-09 18:07:06 +0800

    implement my post,8-3WeiWanDaiXu

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,9 +6,11 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+
+
   def create
     @group = Group.find(params[:group_id])
-    @post = Post.new(post_params)
+    @post = group.new(post_params)
     @post.group = @group
     @post.user = current_user
 
@@ -23,7 +25,34 @@ class PostsController < ApplicationController
   private
 
 
+
   def post_params
     params.require(:post).permit(:content)
   end
 end
+
+=begin
+def edit
+    @group = Post.find(params[:id])
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def delete
+    @post.delete(post_params)
+    flash[:alert] = "Post deleted"
+      redirect_to groups_path(@group)
+  end
+
+  def destroy
+    @post.delete(post_params)
+    flash[:alert] = "Post deleted"
+      redirect_to groups_path(@group)
+  end
+=end
